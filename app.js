77a280efec8898de5ba7a1b3a0f30efe422bba1a
Clement Bourgoin <cb@nokto.net>
2017-11-30 19:04:17 +0100

    Cache response and return cache if available

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 'use strict';
 
 const express = require('express');
-const fetch = require('node-fetch');
+const fs      = require('fs');
+const fetch   = require('node-fetch');
 
 const app = express();
 
 app.get('/books/:ean/shops', async function(request, response) {
 
   const ean = request.params.ean;
+  const date = Date.now();
+  const cacheFile = `./cache/${ean}.json`;
+  let result;
 
-  // Get shop list for this EAN
-  const res  = await fetch(`https://www.placedeslibraires.fr/getshoplist.php?ISBN=${ean}`);
-  const json = await res.json();
+  if (fs.existsSync(cacheFile)) {
+    result = require(cacheFile);
+    process.stdout.write(`Using cached response for EAN ${ean}\n`);
+  }
 
-  // Filter shop list to get only those with available stock
-  const result = json.shop.filter(shop => shop.stockAvailablility);
+  if (typeof result === 'undefined') {
+
+    process.stdout.write(`Getting fresh data for EAN ${ean}\n`);
+
+    // Get shop list for this EAN
+    const res  = await fetch(`https://www.placedeslibraires.fr/getshoplist.php?ISBN=${ean}`);
+    const json = await res.json();
+
+    // Filter shop list to get only those with available stock
+    const shops = json.shop.filter(shop => shop.stockAvailablility);
+
+    result = { ean, date, shops };
+
+    fs.writeFile(cacheFile, JSON.stringify(result), (error) => {
+      if (error) throw error;
+    });
+  }
 
   response.json(result);
 });
 
-app.listen(3000, () => process.stdout.write('Server listening on port 3000!'));
+app.listen(3000, () => process.stdout.write('Server listening on port 3000!\n'));
 
