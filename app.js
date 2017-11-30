a2c489b02ce561de9a18d8513973f81451d6de41
Clement Bourgoin <cb@nokto.net>
2017-11-30 18:04:09 +0100

    Basic server implementation

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 'use strict';
 
+const express = require('express');
 const fetch = require('node-fetch');
 
-(async function() {
+const app = express();
+
+app.get('/shops', async function(request, response) {
 
   // Get shop list for this EAN
   const res  = await fetch('https://www.placedeslibraires.fr/getshoplist.php?ISBN=9782953595109&typeproduit=0&gencod=9782953595109&dispo=1&rid=&geoLat=&geoLon=');
@@ -11,6 +14,8 @@ const fetch = require('node-fetch');
   // Filter shop list to get only those with available stock
   const result = json.shop.filter(shop => shop.stockAvailablility);
 
-  console.log(result);
-})();
+  response.json(result);
+});
+
+app.listen(3000, () => process.stdout.write('Server listening on port 3000!'));
 
