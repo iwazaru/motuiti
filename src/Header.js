ea75af730f2b87ed884c9c2ddaffeb58bbedcfe5
Clement Bourgoin <cb@nokto.net>
2019-01-28 15:49:37 +0100

    Use icons for header buttons

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
-import "./Header.css";
+import './Header.css';
 
 export default function Header({ onSearch, onGeolocate }) {
   function onFormSubmit(event) {
     event.preventDefault();
-    const query = event.target.querySelector(".search-field").value;
+    const query = event.target.querySelector('.search-field').value;
     onSearch(query);
   }
 
@@ -23,9 +23,11 @@ export default function Header({ onSearch, onGeolocate }) {
           type="search"
           placeholder="ISBN du livre"
         />
-        <button type="submit">Rechercher</button>
+        <button type="submit">
+          <span class="fas fa-search" aria-label="Rechercher" />
+        </button>
         <button type="button" onClick={onGeolocateButtonClick}>
-          Me localiser
+          <span class="fas fa-location-arrow" aria-label="Me localiser" />
         </button>
       </form>
     </div>
