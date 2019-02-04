321e5b1ab37db7e6dd01aa389312d8651b6911fe
Clement Bourgoin <cb@nokto.net>
2019-02-04 18:18:51 +0100

    Remove filesystem caching mechanisme from stores API

--- api/stores.js
+++ api/stores.js
@@ -2,40 +2,20 @@ import PDLParser from '../src/utils/PDLParser';
 
 const { parse } = require('url');
 const fetch = require('node-fetch');
-const fs = require('fs');
 
 export default async (req, res) => {
   const { query } = parse(req.url, true);
   const { ean } = query;
-
   const date = Date.now();
-  const cacheFile = `/tmp/${ean}.json`;
-  let result;
 
-  if (fs.existsSync(cacheFile)) {
-    const json = fs.readFileSync(cacheFile);
-    result = JSON.parse(json);
-    process.stdout.write(`Using cached response for EAN ${ean}\n`);
-  }
-
-  if (typeof result === 'undefined') {
-    process.stdout.write(`Getting fresh data for EAN ${ean}\n`);
-
-    // Get shop list for this EAN
-    const res = await fetch(
-      `https://www.placedeslibraires.fr/getshoplist.php?ISBN=${ean}`
-    );
-    const json = await res.json();
-    const storesJson = PDLParser.filterStores(json.shop);
-    const stores = storesJson.map(PDLParser.parseStore);
-
-    result = { ean, date, stores };
-
-    fs.writeFile(cacheFile, JSON.stringify(result), error => {
-      if (error) throw error;
-    });
-  }
+  // Get stores list for this EAN
+  const response = await fetch(
+    `https://www.placedeslibraires.fr/getshoplist.php?ISBN=${ean}`
+  );
+  const json = await response.json();
+  const storesJson = PDLParser.filterStores(json.shop);
+  const stores = storesJson.map(PDLParser.parseStore);
 
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(result));
+  res.end(JSON.stringify({ ean, date, stores }));
 };
