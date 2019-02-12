cfd360f4d5ff8dac6f7bd92feed71753de51c0e7
Clement Bourgoin <cb@nokto.net>
2019-02-12 18:05:47 +0100

    Add version & changelog link in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,16 @@ export default function Header(props) {
   return (
     <div className="Header">
       <div className="title">
-        <Link to="/">motuiti</Link>
+        <Link to="/">motuiti</Link>{' '}
+        <span className="version">
+          <a
+            href="https://github.com/iwazaru/motuiti/releases"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            0.1
+          </a>
+        </span>
       </div>
       <SearchFrom {...props} />
       <Link to="/a-propos">
